369a98c2bd166bedcd7b8547a7b53bf9618af6d7
BryanJ1002 <87389289+BryanJ1002@users.noreply.github.com>
2021-07-13 13:12:20 -0500

    Update Regla_del_Trapecio.m

--- Codigos_integrales/Regla_del_Trapecio.m
+++ Codigos_integrales/Regla_del_Trapecio.m
@@ -1,19 +1,19 @@
-%Limpiamos la pantalla y mostramos el nombre del método
+%Limpiamos la pantalla y mostramos el nombre del mÃ©todo
 clear
 clc
 disp('Regla del trapecio')
-%Damos de alta la variable simbólica X
+%Damos de alta la variable simbÃ³lica X
 syms x
-%Introducimos la función,los puntos a y b, así como el
+%Introducimos la funciÃ³n,los puntos a y b, asÃ­ como el
 %porcentaje de error
-f =input('Introduzca la función f(x):');
+f =input('Introduzca la funciÃ³n f(x):');
 a =input('Introduzca el punto a: ');
 b =input('Introduzca el punto b: ');
-%Graficamos la función en el intervalo [a,b]
+%Graficamos la funciÃ³n en el intervalo [a,b]
 ezplot(f,[a,b]);
 grid,title('\bf Regla del Trapecio')
 f=inline(f);
-%Aproximamos la integral con la fórmula correspondiente
+%Aproximamos la integral con la fÃ³rmula correspondiente
 int=((b-a)/2)*(f(a)+f(b));
 %Mostramos los resultados en pantalla (con 3 decimales)
-fprintf('\nAproximación a la integral=%12.3f \n',int);
+fprintf('\nAproximaciÃ³n a la integral=%12.33f \n',int);
